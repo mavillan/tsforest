18e8da46861f8f4f7fa7a19eed05c9eab4637a8a
Martín Villanueva <martin.villanueva@usm.cl>
2019-08-31 11:20:18 -0400

    Adds a setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+from setuptools import find_packages
+setup(
+    name='tsforest',
+    version='0.1dev',
+    author='Martín Villanueva',
+    author_email='nallivam@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    license='GPLv3',
+)
