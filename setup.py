db5ebd65e14032ed2231bf061a65ad75b441177c
Martín Villanueva <martin.villanueva@walmart.com>
2019-09-03 15:02:26 -0400

    Adds requirements.txt files and updates setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from distutils.core import setup
 from setuptools import find_packages
+
+with open('requirements.txt', 'r') as f:
+    install_requires = f.read().splitlines()
+
 setup(
     name='tsforest',
-    version='0.1dev',
+    version='0.1',
     author='Martín Villanueva',
     author_email='nallivam@gmail.com',
     packages=find_packages(),
+    install_requires=install_requires,
     include_package_data=True,
     license='GPLv3',
 )
