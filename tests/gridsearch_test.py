511433aaf6a918b3f8b079a936274fcbd0130403
Martín Villanueva <martin.villanueva@usm.cl>
2019-09-07 11:39:27 -0400

    Adds tests for grid search training with test data

--- tests/gridsearch_test.py
+++ tests/gridsearch_test.py
@@ -5,33 +5,64 @@ import unittest
 from tsforest.forecaster_lightgbm import LGBMForecaster
 from tsforest.forecaster_gbm import GBMForecaster
 from tsforest.grid_search import GridSearch
+from tsforest.utils import make_time_range
 
 DATA_PATH = './tests/tests_data/data.csv'
 
 class TestGridSearch(unittest.TestCase):
 
     def test_it_fit_with_valid_period_in_lightgbm(self):
-        hyperparams = {'num_leaves':[25,30]}
-        hyperparams_fixed = {'num_iterations':100, 'learning_rate':0.2}
         data = pd.read_csv(DATA_PATH, parse_dates=['ds'])
-        valid_period = data.tail(30).loc[:, ['ds']]
+        train_data = data.query('ds < "2019-06-01"')
+        valid_period = make_time_range('2019-05-01', '2019-05-31', freq='D')
+        hyperparams = {'num_leaves':[25,30]}
+        hyperparams_fixed = {'num_iterations':30, 'learning_rate':0.3}
+  
+        gs = GridSearch(model_class=LGBMForecaster,
+                features=['calendar_mixed'], 
+                hyperparams=hyperparams,
+                hyperparams_fixed=hyperparams_fixed,
+                n_jobs=-1)
+        gs.fit(train_data=train_data, valid_period=valid_period)
+
+    def test_it_fit_with_test_data_in_lightgbm(self):
+        data = pd.read_csv(DATA_PATH, parse_dates=['ds'])
+        train_data = data.query('ds < "2019-06-01"')
+        test_data = data.query('ds >= "2019-06-01"')
+        hyperparams = {'num_leaves':[25,30]}
+        hyperparams_fixed = {'num_iterations':30, 'learning_rate':0.3}
         
         gs = GridSearch(model_class=LGBMForecaster,
                 features=['calendar_mixed'], 
                 hyperparams=hyperparams,
                 hyperparams_fixed=hyperparams_fixed,
                 n_jobs=-1)
-        gs.fit(data, valid_period)
+        gs.fit(data, test_data=test_data)
 
     def test_it_fit_with_valid_period_in_gbm(self):
-        hyperparams = {'max_depth':[5,7]}
-        hyperparams_fixed = {'ntrees':100, 'learn_rate':0.2}
         data = pd.read_csv(DATA_PATH, parse_dates=['ds'])
-        valid_period = data.tail(30).loc[:, ['ds']]
-        
+        train_data = data.query('ds < "2019-06-01"')
+        valid_period = make_time_range('2019-05-01', '2019-05-31', freq='D')
+        hyperparams = {'max_depth':[5,7]}
+        hyperparams_fixed = {'ntrees':30, 'learn_rate':0.3}
+
         gs = GridSearch(model_class=GBMForecaster,
                 features=['calendar_mixed'], 
                 hyperparams=hyperparams,
                 hyperparams_fixed=hyperparams_fixed,
                 n_jobs=-1)
-        gs.fit(data, valid_period)
+        gs.fit(train_data=train_data, valid_period=valid_period)
+
+    def test_it_fit_with_test_data_in_gbm(self):
+        data = pd.read_csv(DATA_PATH, parse_dates=['ds'])
+        train_data = data.query('ds < "2019-06-01"')
+        test_data = data.query('ds >= "2019-06-01"')
+        hyperparams = {'max_depth':[5,7]}
+        hyperparams_fixed = {'ntrees':30, 'learn_rate':0.3}
+
+        gs = GridSearch(model_class=GBMForecaster,
+                features=['calendar_mixed'], 
+                hyperparams=hyperparams,
+                hyperparams_fixed=hyperparams_fixed,
+                n_jobs=-1)
+        gs.fit(train_data=train_data, test_data=test_data)
