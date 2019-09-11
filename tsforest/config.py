ab4387cce15985d47200be2c0fb964b9c4239614
Martín Villanueva <martin.villanueva@walmart.com>
2019-09-11 14:03:24 -0300

    Removes unused features types from config.py

--- tsforest/config.py
+++ tsforest/config.py
@@ -20,6 +20,7 @@ lgbm_parameters = {
     'boosting_type':'gbrt',
     'objective':'regression',
     'num_leaves':31,
+    'min_data_in_leaf':20,
     'learning_rate':0.1,
     'feature_fraction':0.9,
     'num_iterations':500
@@ -28,7 +29,7 @@ lgbm_parameters = {
 # fbprophet default parameters 
 prophet_kwargs = {
     'n_changepoints':120,
-    'changepoint_range':0.97, 
+    'changepoint_range':0.9,
     'changepoint_prior_scale':0.05
 }
 prophet_kwargs_extra = {
@@ -41,9 +42,12 @@ prophet_kwargs_extra = {
 # datatype of sequential calendar features
 calendar_sequential_features_types = {
     'week_day':'numeric',
+    'month_day':'numeric',
     'year_day':'numeric',
     'year_week':'numeric',
-    'month':'categorical'
+    'month':'categorical',
+    'year':'numeric',
+    'month_progress':'numeric'
 }
 
 # datatype of cyclical calendar features
@@ -58,50 +62,8 @@ calendar_cyclical_features_types = {
     'month_sin':'numeric'
 }
 
-#  datatype of general calendar features
-calendar_general_features_types = {
-    'month_day':'numeric',
-    'month_progress':'numeric',
-    'year':'numeric',
-    'sequence_day':'numeric',
-    'fsl_type':'categorical',
-    'fsl_day':'categorical',
-    'prev_fsl':'categorical',
-    'post_fsl':'categorical'
-}
-
-# datatype of events features
-events_features_types = {
-    'closed':'numeric',
-    'holiday':'categorical',
-    'prev_christmas':'categorical',
-    'prev_newyear':'categorical',
-    'prev_18':'categorical',
-    'prev_trabajo':'categorical',
-    'mil_a_mil':'categorical',
-    'bf':'categorical',
-    'ss_fds':'categorical',
-    'prev_ss_fds':'categorical',
-    'prev_closed':'categorical', 
-    'prev_holiday':'categorical'
-}
-
-# datatype of store opening features
-store_opening_features_types = {
-    'fecha':'numeric',
-    'mts_sq_bodega':'numeric',
-    'mts_sq_ekono':'numeric',
-    'mts_sq_express':'numeric',
-    'mts_sq_lider':'numeric',
-    'mts_sq_mayorista':'numeric',
-    'mts_sq_total':'numeric'
-}
-
 # datatype of all features
 all_features_types = {
     **calendar_sequential_features_types,
-    **calendar_cyclical_features_types,
-    **calendar_general_features_types,
-    **events_features_types,
-    **store_opening_features_types
+    **calendar_cyclical_features_types
 }
