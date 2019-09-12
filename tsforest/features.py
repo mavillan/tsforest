cc3094d05289a926deb92cfaba740253c6c93095
Martín Villanueva <martin.villanueva@usm.cl>
2019-09-12 18:31:01 -0300

    Adds support for time series data with time gaps

--- tsforest/features.py
+++ tsforest/features.py
@@ -8,6 +8,21 @@ from tsforest.config import (calendar_sequential_features_types,
                              calendar_cyclical_features_types,
                              all_features_types)
 
+def fill_time_gaps(data):
+    """
+    Parameters
+    ----------
+    data: pandas.DataFrame
+        dataframe with columns 'ds' (dtype datetime64) and 'y' 
+    """
+    assert set(['ds','y']) <= set(data.columns), \
+        'data must contain the column "ds"'
+    filled_data = (data
+                   .resample("D", on='ds').y.mean()
+                   .interpolate("linear")
+                   .reset_index())
+    filled_data = pd.merge(filled_data, data.drop('y', axis=1), on=['ds'], how='left')
+    return filled_data
 
 def compute_calendar_features(start_time, end_time, freq='D'):
     """
@@ -60,8 +75,8 @@ def compute_lag_features(data, lags):
         'Missing "y" column in dataframe'
     features = [data.y.shift(lag) for lag in lags]
     features_names = [f'lag_{lag}' for lag in lags]
-    lag_features = pd.concat(features, axis=1)
-    lag_features.columns = features_names
+    lag_features = pd.concat([data.ds] + features, axis=1)
+    lag_features.columns = ['ds'] + features_names
     return lag_features  
 
 def compute_rw_features(data, window_sizes, window_functions):
@@ -83,8 +98,8 @@ def compute_rw_features(data, window_sizes, window_functions):
     features_names = [f'{window_func}_{window}'
                       for window_func in window_functions
                       for window in window_sizes]
-    rw_features = pd.concat(features, axis=1)
-    rw_features.columns = features_names
+    rw_features = pd.concat([data.ds] + features, axis=1)
+    rw_features.columns = ['ds'] + features_names
     return rw_features
 
 class FeaturesGenerator():
@@ -117,16 +132,14 @@ class FeaturesGenerator():
         Returns
         ----------
         all_features: pd.Dataframe
-            Dataframe containing all the features for the time serie
-
+            Dataframe containing all the features for the time series
         """
         self.train_data = data
-        # todo: assert that data.ds is a continuous time range
+        # in case of time gaps
+        filled_data = fill_time_gaps(data)
 
         # list with all the dataframes of features
         all_features_list = list()
-        # time range index of the input data
-        data_time_range = pd.DataFrame(index=data.ds.values)
 
         # generating the calendar features
         if np.any(['calendar' in feat for feat in self.include_features]):
@@ -141,15 +154,16 @@ class FeaturesGenerator():
         all_features_list.append(calendar_features.set_index('ds'))
                 
         if "lag" in self.include_features:
-            lag_features = compute_lag_features(data, lags=self.lags)
-            lag_features.set_index(data_time_range.index, inplace=True)
-            all_features_list.append(lag_features)
-    
+            lag_features = (compute_lag_features(filled_data, lags=self.lags)
+                            .merge(data.loc[:, ['ds']], how='inner', on=['ds']))
+            all_features_list.append(lag_features.set_index('ds'))
+
         if "rw" in self.include_features:
-            rw_features = compute_rw_features(data, window_sizes=self.window_sizes, 
-                                              window_functions=self.window_functions)
-            rw_features.set_index(data_time_range.index, inplace=True)
-            all_features_list.append(rw_features)
+            rw_features = (compute_rw_features(filled_data, 
+                                               window_sizes=self.window_sizes, 
+                                               window_functions=self.window_functions)
+                           .merge(data.loc[:, ['ds']], how='inner', on=['ds']))
+            all_features_list.append(rw_features.set_index('ds'))
 
         # merging all features
         all_features_list.append(data.set_index("ds"))
@@ -172,12 +186,11 @@ class FeaturesGenerator():
 
         # list with all the dataframes of features
         all_features_list = list()
-        # time index of the input data
-        data_time_range = pd.DataFrame(index=data.ds.values)
 
         # generating the calendar features
         if np.any(['calendar' in feat for feat in self.include_features]):
-            calendar_features = compute_calendar_features(data.ds.min(), data.ds.max())
+            calendar_features = (compute_calendar_features(data.ds.min(), data.ds.max())
+                                 .merge(data.loc[:, ['ds']], how='inner', on=['ds']))
         if "calendar_sequential" in self.include_features:
             columns_to_drop = list(calendar_cyclical_features_types.keys())
             calendar_features.drop(columns=columns_to_drop, inplace=True)
@@ -188,7 +201,7 @@ class FeaturesGenerator():
 
         if "lag" in self.include_features:
             column_names = [f'lag_{lag}' for lag in self.lags]
-            lag_features = pd.DataFrame(np.nan, index=data_time_range.index, 
+            lag_features = pd.DataFrame(np.nan, index=data.ds, 
                                         columns=column_names)
             all_features_list.append(lag_features)
     
@@ -196,7 +209,7 @@ class FeaturesGenerator():
             column_names = [f'{window_func}_{window}'
                             for window_func in self.window_functions
                             for window in self.window_sizes]
-            rw_features = pd.DataFrame(np.nan, index=data_time_range.index,
+            rw_features = pd.DataFrame(np.nan, index=data.ds,
                                        columns=column_names)
             all_features_list.append(rw_features)
         
