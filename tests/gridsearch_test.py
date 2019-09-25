ac1478620a35f0b2b47001f425a5c78649539cfa
Martín Villanueva <martin.villanueva@usm.cl>
2019-09-25 20:59:00 -0300

    Renames the classes of forecaster models

--- tests/gridsearch_test.py
+++ tests/gridsearch_test.py
@@ -2,8 +2,8 @@ import numpy as np
 import pandas as pd
 import unittest
 
-from tsforest.forecaster_lightgbm import LGBMForecaster
-from tsforest.forecaster_gbm import GBMForecaster
+from tsforest.forecaster_lightgbm import LightGBMForecaster
+from tsforest.forecaster_h2ogbm import H2OGBMForecaster
 from tsforest.grid_search import GridSearch
 from tsforest.utils import make_time_range
 
@@ -18,7 +18,7 @@ class TestGridSearch(unittest.TestCase):
         hyperparams = {'num_leaves':[25,30]}
         hyperparams_fixed = {'num_iterations':30, 'learning_rate':0.3}
   
-        gs = GridSearch(model_class=LGBMForecaster,
+        gs = GridSearch(model_class=LightGBMForecaster,
                 features=['calendar_mixed'], 
                 hyperparams=hyperparams,
                 hyperparams_fixed=hyperparams_fixed,
@@ -32,35 +32,35 @@ class TestGridSearch(unittest.TestCase):
         hyperparams = {'num_leaves':[25,30]}
         hyperparams_fixed = {'num_iterations':30, 'learning_rate':0.3}
         
-        gs = GridSearch(model_class=LGBMForecaster,
+        gs = GridSearch(model_class=LightGBMForecaster,
                 features=['calendar_mixed'], 
                 hyperparams=hyperparams,
                 hyperparams_fixed=hyperparams_fixed,
                 n_jobs=-1)
         gs.fit(data, test_data=test_data)
 
-    def test_it_fit_with_valid_period_in_gbm(self):
+    def test_it_fit_with_valid_period_in_h2ogbm(self):
         data = pd.read_csv(DATA_PATH, parse_dates=['ds'])
         train_data = data.query('ds < "2019-06-01"')
         valid_period = make_time_range('2019-05-01', '2019-05-31', freq='D')
         hyperparams = {'max_depth':[5,7]}
         hyperparams_fixed = {'ntrees':30, 'learn_rate':0.3}
 
-        gs = GridSearch(model_class=GBMForecaster,
+        gs = GridSearch(model_class=H2OGBMForecaster,
                 features=['calendar_mixed'], 
                 hyperparams=hyperparams,
                 hyperparams_fixed=hyperparams_fixed,
                 n_jobs=-1)
         gs.fit(train_data=train_data, valid_period=valid_period)
 
-    def test_it_fit_with_test_data_in_gbm(self):
+    def test_it_fit_with_test_data_in_h2ogbm(self):
         data = pd.read_csv(DATA_PATH, parse_dates=['ds'])
         train_data = data.query('ds < "2019-06-01"')
         test_data = data.query('ds >= "2019-06-01"')
         hyperparams = {'max_depth':[5,7]}
         hyperparams_fixed = {'ntrees':30, 'learn_rate':0.3}
 
-        gs = GridSearch(model_class=GBMForecaster,
+        gs = GridSearch(model_class=H2OGBMForecaster,
                 features=['calendar_mixed'], 
                 hyperparams=hyperparams,
                 hyperparams_fixed=hyperparams_fixed,
