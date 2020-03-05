45cd6e96bf8046dff810aae70f16cb443e3dccf9
Martín Villanueva <martin.villanueva@walmart.com>
2020-03-05 13:54:30 -0300

    Updates package version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt', 'r') as f:
 
 setup(
     name='tsforest',
-    version='0.2.23',
+    version='0.2.24',
     author='Martín Villanueva',
     author_email='nallivam@gmail.com',
     packages=find_packages(),
