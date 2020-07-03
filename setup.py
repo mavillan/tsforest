1f66f9cdeb19794743630a3896f5850e4e1f8407
Martín Villanueva <martin.villanueva@usm.cl>
2020-07-02 22:09:22 -0500

    Updates package version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt', 'r') as f:
 
 setup(
     name='tsforest',
-    version='0.2.48',
+    version='0.2.49',
     author='Martín Villanueva',
     author_email='nallivam@gmail.com',
     packages=find_packages(),
