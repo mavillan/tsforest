2912bcaa1408d1914a96014765f12436cd60288b
Martín Villanueva <mavillan@spikelab.xyz>
2021-08-12 00:02:46 -0500

    Updates package version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt', 'r') as f:
 
 setup(
     name='tsforest',
-    version='0.2.49',
+    version='0.2.50',
     author='Martín Villanueva',
     author_email='nallivam@gmail.com',
     packages=find_packages(),
